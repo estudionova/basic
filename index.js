c948216044cd6277ab6f2ec227929286402910c1
Reinaldo Junior <rsljunior@live.com>
2022-03-15 11:12:32 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ * Reinaldo junior
+ * Primeiro contato com javascript
+ * 14/03/2022
+ */
+
+
+let n = 7;
+
+for( let i = 0; i <= 10; i++){
+
+    console.log(`${i} X ${n} = ${i * n} `);
+
+}
+
+
+/*
+let cor = "azul";
+switch (cor) {
+    case "verde":
+        console.log('Siga');
+        break;
+
+    case "amarelo":
+        console.log('atenção');
+        break;
+
+    case "vermelho":
+        console.log('pare');
+        break;
+
+    default:
+        console.log('não sei');
+}
+
+
+let cor = "Vermelho";
+
+if (cor === 'Verde') {
+    console.log('Siga');
+} else if (cor === "Amarelo") {
+    console.log("Atenção");
+} else if (cor === "Vermelho") {
+    console.log('Pare');
+}
+
+
+var olaMundo = 'Olá Mundo!';
+
+let a = 10;
+const b = "10";
+
+console.log( a == b || typeof a == 'string');
+
+console.log(olaMundo);
+console.log('Olá Mundo!');
+console.log('Olá Mundo!');
+//alert('Olá Mundo!');
+*/
